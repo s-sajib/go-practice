f52b791d64cf775fb7218eee3791253f0db06e87
Md. Shohidujjaman Sajib <sajibwebdev@gmail.com>
2024-05-25 18:53:23 +0600

    new: maps and pointers

--- maps/maps.go
+++ maps/maps.go
@@ -2,22 +2,12 @@
 //  Write a program to display server status.
 //
 //--Requirements:
-//* Create a function to print server status, including:
-//  - Number of servers
-//  - Number of servers for each status (Online, Offline, Maintenance, Retired)
-//* Store the existing slice of servers in a map
-//* Default all of the servers to `Online`
-//* Perform the following status changes and display server info:
-//  - display server info
-//  - change `darkstar` to `Retired`
-//  - change `aiur` to `Offline`
-//  - display server info
-//  - change all servers to `Maintenance`
-//  - display server info
 
 package main
 
 import "fmt"
+
+type Server map[string]int
 
 const (
 	Online      = 0
@@ -26,6 +16,45 @@ const (
 	Retired     = 3
 )
 
+// Creating status map for making readable Statuses
+
+var statusMap = map[int]string{0: "online", 1: "Offline", 2: "Maintenance", 3: "Retired"}
+
+func serversByStatus(servers Server) {
+	fmt.Println("Total Number of Servers: ", len(servers))
+
+	statusCount := make(map[string]int)
+
+	for _, status := range servers {
+		readableStatus := statusMap[status]
+		statusCount[readableStatus] += 1
+	}
+	fmt.Println(statusCount)
+}
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	//* Store the existing slice of servers in a map
+	//* Default all of the servers to `Online`
+	serverMap := make(Server)
+	for _, server := range servers {
+		serverMap[server] = 0
+	}
+
+	// * Perform the following status changes and display server info:
+	//   - display server info
+	serversByStatus(serverMap)
+	// - change `darkstar` to `Retired`
+	serverMap["darkstar"] = 3
+	// - change `aiur` to `Offline`
+	serverMap["aiur"] = 1
+	// - display server info
+	serversByStatus(serverMap)
+	// - change all servers to `Maintenance`
+	for name := range serverMap {
+		serverMap[name] = 2
+	}
+	// - display server info
+	serversByStatus(serverMap)
+
 }
--- pointers/pointers.go
+++ pointers/pointers.go
@@ -3,21 +3,65 @@
 //  on products.
 //
 //--Requirements:
-//* Create a structure to store items and their security tag state
-//  - Security tags have two states: active (true) and inactive (false)
-//* Create functions to activate and deactivate security tags using pointers
-//* Create a checkout() function which can deactivate all tags in a slice
-//* Perform the following:
-//  - Create at least 4 items, all with active security tags
-//  - Store them in a slice or array
-//  - Deactivate any one security tag in the array/slice
-//  - Call the checkout() function to deactivate all tags
-//  - Print out the array/slice after each change
 
 package main
 
 import "fmt"
 
+var (
+	Active   = true
+	Inavtive = false
+)
+
+// * Create a structure to store items and their security tag state
+//   - Security tags have two states: active (true) and inactive (false)
+type SecurityTag struct {
+	name string
+	tag  bool
+}
+
+//* Create functions to activate and deactivate security tags using pointers
+
+func activateTag(tag *bool) {
+	*tag = Active
+}
+func deActivateTag(tag *bool) {
+	*tag = Inavtive
+
+}
+
+// * Create a checkout() function which can deactivate all tags in a slice
+func checkout(tags []SecurityTag) {
+	for i := 0; i < len(tags); i++ {
+		/**
+		lessons Learnt: if you range over a slice/array, you're doing operation on the copy of the slice.
+		*/
+		deActivateTag(&tags[i].tag)
+	}
+}
+
 func main() {
 
+	//* Perform the following:
+	//  - Create at least 4 items, all with active security tags
+
+	tag1 := SecurityTag{name: "Tag 1", tag: Active}
+	tag2 := SecurityTag{name: "Tag 2", tag: Active}
+	tag3 := SecurityTag{name: "Tag 3", tag: Active}
+	tag4 := SecurityTag{name: "Tag 4", tag: Active}
+
+	//  - Store them in a slice or array
+	tags := []SecurityTag{tag1, tag2, tag3, tag4}
+	fmt.Println("All Tags:")
+	fmt.Println(tags)
+
+	//  - Deactivate any one security tag in the array/slice
+	fmt.Println("Deactivated Tags[0]:")
+	deActivateTag(&tags[0].tag)
+	fmt.Println(tags)
+
+	//  - Call the checkout() function to deactivate all tags
+	fmt.Println("Checking Out Tags:")
+	checkout(tags)
+	fmt.Println(tags)
 }
